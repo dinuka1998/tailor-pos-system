64bad6c46dbc5f02e016f455d74689bbd43e006e
KavinduDan <dananjayakavinduedu@gmail.com>
2021-12-04 22:55:19 +0530

    customer home ui

---
+++ Employee/CustomerHome.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer Home</title>
+    <link rel="stylesheet" href="css/customerStyles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9874d4ab7b.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="username">
+
+        </div>
+        <div class="row btn-container">
+            <div class="col-md-4 col-xl-4">
+                <a href="page.html">
+                    <div class="card bg-c-blue order-card">
+                        <div class="card-block">
+                            <h2 class="text-center "><span><i class="fas fa-shopping-cart btn-icon"></i>Orders</span></h2>
+                            <p class="m-b-0 text-center extra-text">order informations</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-4 col-xl-4">
+                <a href="page.html">
+                    <div class="card bg-c-green order-card">
+                        <div class="card-block">
+                            <h2 class="text-center "><i class="fas fa-envelope btn-icon"></i><span>Masseges</span></h2>
+                            <p class="m-b-0 text-center extra-text">Masseges from tailor</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-4 col-xl-4">
+                <a href="page.html">
+                    <div class="card bg-c-yellow order-card">
+                        <div class="card-block">
+                            <h2 class="text-center "><i class="fas fa-calendar-check btn-icon"></i></i><span>Appointments</span></h2>
+                            <p class="m-b-0 text-center extra-text">create appointments</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
--- Employee/customeradd.php
+++ Employee/customeradd.php
@@ -9,52 +9,51 @@ if (!is_user()) {
 
 ?>
 
-		
+
 
 <?php
- $user = $_SESSION['username'];
-$usid = $pdo->query("SELECT id FROM users WHERE username='".$user."'");
+$user = $_SESSION['username'];
+$usid = $pdo->query("SELECT id FROM users WHERE username='" . $user . "'");
 $usid = $usid->fetch(PDO::FETCH_ASSOC);
- $uid = $usid['id'];
- include ('header.php');
- ?>
+$uid = $usid['id'];
+include('header.php');
+?>
 
 
 
- 
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Add Customer</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                
-                <div class="col-md-10 col-md-offset-1">
-				
-				
-	
 
-		<?php
+<div id="page-wrapper">
+	<div class="row">
+		<div class="col-lg-12">
+			<h1 class="page-header">Add Customer</h1>
+		</div>
+		<!-- /.col-lg-12 -->
+	</div>
+	<!-- /.row -->
+	<div class="row">
 
-if($_POST)
-{
+		<div class="col-md-10 col-md-offset-1">
 
-$fullname = $_POST["fullname"];
-$address = $_POST["address"];
-$phonenumber = $_POST["phonenumber"];
-$sex = $_POST["sex"];
-$email = $_POST["email"];
-$city = $_POST["city"];
-$comment = $_POST["comment"];
 
-$res = $pdo->exec("INSERT INTO customer SET fullname='".$fullname."', address='".$address."', phonenumber='".$phonenumber."', sex='".$sex."',`email`='".$email."',`city`='".$city."',`comment`='".$comment."'");
-$cid = $pdo->lastInsertId();
-if($res){
 
-echo "<div class='alert alert-success alert-dismissable'>
+
+			<?php
+
+			if ($_POST) {
+
+				$fullname = $_POST["fullname"];
+				$address = $_POST["address"];
+				$phonenumber = $_POST["phonenumber"];
+				$sex = $_POST["sex"];
+				$email = $_POST["email"];
+				$city = $_POST["city"];
+				$comment = $_POST["comment"];
+
+				$res = $pdo->exec("INSERT INTO customer SET fullname='" . $fullname . "', address='" . $address . "', phonenumber='" . $phonenumber . "', sex='" . $sex . "',`email`='" . $email . "',`city`='" . $city . "',`comment`='" . $comment . "'");
+				$cid = $pdo->lastInsertId();
+				if ($res) {
+
+					echo "<div class='alert alert-success alert-dismissable'>
 <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>	
 
 Customer Added Successfully!
@@ -62,88 +61,84 @@ Customer Added Successfully!
 </div>
 <meta http-equiv='refresh' content='2; url=addmeasurement.php?id=$cid' /> 
 ";
+				}
+			}
+			?>
 
 
-}
+
+			<script>
+				$(function() {
+					$("#datepicker").datepicker();
+				});
+			</script>
 
 
 
-} 
-	?>
-		
 
 
-	 <script>
-  $(function() {
-    $( "#datepicker" ).datepicker();
-  });
-  </script>		
-				
-				
-				
-				
-				
-				    <form action="customeradd.php" method="post">
-		
-                    
-                <div class="form-group">
-					
-					<label>Full Name</label><br/>
-                 	<input type="text" name="fullname" style="width:200px; height: 40px;" /><br/><br/>
-				</div>  
-                
-                <div class="form-group">
-					
-					<label>Address</label><br/>
-                 	<input type="text" name="address" style="width:200px; height: 40px;" /><br/><br/>
-				</div>    
-                
-                <div class="form-group">
-					
-					<label>Phone Number</label><br/>
-                 	<input type="text" name="phonenumber" style="width:200px; height: 40px;" /><br/><br/>
-				</div>
-				
+			<form action="customeradd.php" method="post">
+
+
 				<div class="form-group">
-					
-					<label>City</label><br/>
-                 	<input type="text" name="city" style="width:200px; height: 40px;" /><br/><br/>
+
+					<label>Full Name</label><br />
+					<input type="text" name="fullname" style="width:200px; height: 40px;" /><br /><br />
 				</div>
-				
+
 				<div class="form-group">
-					
-					<label>Email</label><br/>
-                 	<input type="text" name="email" style="width:200px; height: 40px;" /><br/><br/>
+
+					<label>Address</label><br />
+					<input type="text" name="address" style="width:200px; height: 40px;" /><br /><br />
 				</div>
-                
+
 				<div class="form-group">
-				  <label class="col-sm-3 control-label">Comment</label><br/><br/>
-				  <div class="col-sm-6"><textarea rows="4" cols="50" name="comment" class="form-control" type="text" /></textarea></div>
+
+					<label>Phone Number</label><br />
+					<input type="text" name="phonenumber" style="width:200px; height: 40px;" /><br /><br />
 				</div>
-                
-                <div class="form-group">
-					
-					<label>Sex</label><br/>
-					
+
+				<div class="form-group">
+
+					<label>City</label><br />
+					<input type="text" name="city" style="width:200px; height: 40px;" /><br /><br />
+				</div>
+
+				<div class="form-group">
+
+					<label>Email</label><br />
+					<input type="text" name="email" style="width:200px; height: 40px;" /><br /><br />
+				</div>
+
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Comment</label><br /><br />
+					<div class="col-sm-6"><textarea rows="4" cols="50" name="comment" class="form-control" type="text" /></textarea></div>
+				</div>
+
+				<div class="form-group">
+
+					<label>Sex</label><br />
+
 					<select name="sex" class="form-control">
-					<option value="0">Male</option>
-                    <option value="1">Female</option></select><br/><br/>
+						<option value="0">Male</option>
+						<option value="1">Female</option>
+					</select><br /><br />
 				</div>
-                
-					<input type="submit" class="btn btn-lg btn-success btn-block" value="ADD">
-			    	</form>
-                </div>
-						
-						
-						
-						
-						
-				
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /#page-wrapper -->
-	    
+
+				<input type="submit" class="btn btn-lg btn-success btn-block" value="ADD">
+			</form>
+		</div>
+
+
+
+
+
+
+	</div>
+	<!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
+
 
 
 
@@ -151,18 +146,24 @@ Customer Added Successfully!
 
 
 <script>
-jQuery(document).ready(function(){
-    
-  
-  jQuery("#ssn").mask("999-99-9999");
-  
-  // Time Picker
-  jQuery('#timepicker').timepicker({defaultTIme: false});
-  jQuery('#timepicker2').timepicker({showMeridian: false});
-  jQuery('#timepicker3').timepicker({minuteStep: 15});
+	jQuery(document).ready(function() {
 
-  
-});
+
+		jQuery("#ssn").mask("999-99-9999");
+
+		// Time Picker
+		jQuery('#timepicker').timepicker({
+			defaultTIme: false
+		});
+		jQuery('#timepicker2').timepicker({
+			showMeridian: false
+		});
+		jQuery('#timepicker3').timepicker({
+			minuteStep: 15
+		});
+
+
+	});
 </script>
 
 
@@ -172,5 +173,5 @@ jQuery(document).ready(function(){
 
 
 <?php
- include ('footer.php');
- ?>
+include('footer.php');
+?>
