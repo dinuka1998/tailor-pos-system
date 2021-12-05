d0937eb6cd98a23b1a0025dec74ead327ea675f8
KavinduDan <dananjayakavinduedu@gmail.com>
2021-12-05 12:58:52 +0530

    added garment list and single product pages

---
+++ Employee/productList.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Popular Prodcuts</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9874d4ab7b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/productList.css">
+</head>
+
+<body>
+
+    <?php include("customernav.php"); ?>
+
+    <div class="container bootdey">
+        <div class="page-title">
+            <h1>Popular Garments</h1>
+        </div>
+        <div class="col-md-12">
+            <div class="row product-list">
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/suite-1.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Gray blazer with glen check pattern
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/suite-2.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Heavy wool blazer in gray melange fishbone weave
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/suite-3.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Three-piece suit in gray with a classic check pattern
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/suite-4.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Two-piece blue suit in houndstooth weave
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/suite-5.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Two-piece classic tuxedo in black fine twill
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/polo-1.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    White short-sleeved polo shirt
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/polo-2.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Black short-sleeved polo shirt
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/polo-3.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Emerald green long-sleeved polo shirt
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/polo-4.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Navy long-sleeved polo shirt
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/pant-1.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Navy stretch chinos
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/pant-2.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Rust chinos
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/pant-3.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Burgundy chinos
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-3">
+                    <section class="panel">
+                        <div class="pro-img-box">
+                            <img src="./assets/img/garments/pant-4.jpg" alt="" />
+                            <a href="#" class="adtocart">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </a>
+                        </div>
+
+                        <div class="panel-body text-center">
+                            <h4>
+                                <a href="#" class="pro-title">
+                                    Dark green shorts
+                                </a>
+                            </h4>
+                            <p class="price">$300.00</p>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ Employee/singleProduct.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/9874d4ab7b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/singleProduct.css">
+</head>
+
+<body>
+
+    <?php include("customernav.php"); ?>
+
+    <div class="container bootdey">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6 image-area">
+                    <div class="pro-img-details">
+                        <img src="./assets/img/garments/suite-1.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-6 details-area">
+                    <h4 class="pro-d-title">
+                        Gray blazer with glen check pattern
+
+                    </h4>
+                    <p>
+                        Burgundy chinos in 100% cotton, woven in durable twill weave. The chinos have a metal zipper fly, straight side pockets and one back vent pocket. Add a personal touch by embroider your initials on these chinos. Made to your measurements.
+                    </p>
+                    <div class="product_meta">
+                        <span class="posted_in"> <strong>Categories:</strong> <a rel="tag" href="#">Jackets</a>, <a rel="tag" href="#">Men</a>, <a rel="tag" href="#">Shirts</a>, <a rel="tag" href="#">T-shirt</a>.</span>
+                        <span class="tagged_as"><strong>Tags:</strong> <a rel="tag" href="#">mens</a>, <a rel="tag" href="#">womens</a>.</span>
+                    </div>
+                    <div class="m-bot15"> <strong>Price : </strong> <span class="amount-old">$544</span> <span class="pro-price"> $300.00</span></div>
+                    <div class="form-group row">
+                        <label for="quantiy" class="col-sm-2 col-form-label">Quantity</label>
+                        <div class="col-sm-10">
+                            <input type="quantiy" class="form-control" id="quantiy" placeholder="1">
+                        </div>
+                    </div>
+                    <p>
+                        <button class="btn btn-round btn-danger" type="button"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
