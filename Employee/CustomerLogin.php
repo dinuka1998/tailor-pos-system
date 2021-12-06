ce93e361ce6e77d87051782ebe9c17172c201c6f
KavinduDan <dananjayakavinduedu@gmail.com>
2021-12-06 15:24:42 +0530

    customer sign up styles

--- Employee/CustomerLogin.php
+++ Employee/CustomerLogin.php
@@ -1,135 +1,138 @@
 <?php
 if (file_get_contents('function.php') == '') {
-	header('Location: '.'/install/');
-} 
+    header('Location: ' . '/install/');
+}
 require_once('function.php');
 session_start();
 
 function is_custm()
 {
-  if (isset($_SESSION['cid']))
-    return true;
+    if (isset($_SESSION['cid']))
+        return true;
 }
 
 if (is_custm()) {
-	redirect('CustomerHome.php');
+    redirect('CustomerHome.php');
 }
 
 
 if (is_user()) {
-	redirect('orderlist.php');
+    redirect('orderlist.php');
 }
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Sign In</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sign In</title>
 
-        <!-- CSS -->
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
-		<link rel="stylesheet" href="assets/css/form-elements.css">
-        <link rel="stylesheet" href="assets/css/style.css">
-<style>
+    <!-- CSS -->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/form-elements.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+        .pass {
+            width: 100%;
+            height: 50px;
+        }
 
-.pass {
-    width: 100%;
-    height: 50px;
-}
-body{
-    background:#00c2a5;
-}
-.signup-text{
-    color:black;
-    font-size: 15px;
-}
-</style>
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
+        body {
+            background: #00c2a5;
+        }
+
+        .signup-text {
+            color: black;
+            font-size: 15px;
+        }
+    </style>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
 
 
-    </head>
+</head>
 
-    <body>
+<body>
 
-		<!-- Top menu -->
-		
-        <!-- Top content -->
-        <div class="top-content">
-        	
-            <div class="inner-bg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-8 col-sm-offset-2 text">
-                            <h1><strong>Customer </strong> Login</h1>
-                        </div>
+    <!-- Top menu -->
+
+    <!-- Top content -->
+    <div class="top-content">
+
+        <div class="inner-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2 text">
+                        <h1><strong>Customer </strong> Login</h1>
                     </div>
-                    <div class="row">
+                </div>
+                <div class="row">
 
-                        <div class="col-md-6 col-md-offset-3 form-box">
-                        	<div class="form-top">
-                        		<div class="form-top-left">
-                        			<h4 style="color:white">Sign In</h4>
-                            		
-                        		</div>
-                        		<div class="form-top-right">
-                        			<i class="fa fa-user"></i>
-                        		</div>
+                    <div class="col-md-6 col-md-offset-3 form-box">
+                        <div class="form-top">
+                            <div class="form-top-left">
+                                <h4 style="color:white">Sign In</h4>
+
                             </div>
-                            <div class="form-bottom">
-			                    
-								
-								               		
-						<?php if (!empty($_GET['error'])): ?>
-	                              <div class="alert alert-danger alert-dismissable">
-                                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>	
-							      <?php echo $_GET['error']?>
-                                  </div>
-                                <?php endif ?>
-								
-								
-								<form role="form" action="CustomerSigninController.php" method="post" class="registration-form">
-								
+                            <div class="form-top-right">
+                                <i class="fa fa-user"></i>
+                            </div>
+                        </div>
+                        <div class="form-bottom">
+
+
+
+                            <?php if (!empty($_GET['error'])) : ?>
+                                <div class="alert alert-danger alert-dismissable">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                    <?php echo $_GET['error'] ?>
+                                </div>
+                            <?php endif ?>
+
+
+                            <form role="form" action="CustomerSigninController.php" method="post" class="registration-form">
+
                                 <div class="form-group">
-                                <input type="email" name="email" value="admin" class="form-first-name form-control" placeholder="Enter your email here..">
+                                    <input type="email" name="email" value="admin" class="form-first-name form-control" placeholder="Enter your email here..">
                                 </div>
                                 <div class="form-group">
-                                <input type="password" name="password" value="admin" class="pass form-control" placeholder="Enter your password here..">
+                                    <input type="password" name="password" value="admin" class="pass form-control" placeholder="Enter your password here..">
                                 </div>
 
-			                        <button type="submit" class="btn"> Submit</button>
-			                    </form>
-		                    </div>
-                            <div class="signup-text">  If you dont have an account <a href="CustomerSignUp.php" style="color:red">SignUp </a> </div>   
+                                <button type="submit" class="btn"> Submit</button>
+                            </form>
                         </div>
+                        <div class="signup-text"> If you dont have an account <a href="CustomerSignUp.php" style="color:red">SignUp </a> </div>
                     </div>
                 </div>
             </div>
-            
         </div>
 
+    </div>
 
-        <!-- Javascript -->
-        <script src="assets/js/jquery-1.11.1.min.js"></script>
-        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/js/jquery.backstretch.min.js"></script>
-        <script src="assets/js/retina-1.1.0.min.js"></script>
-        <script src="assets/js/scripts.js"></script>
-        
-        <!--[if lt IE 10]>
+
+    <!-- Javascript -->
+    <script src="assets/js/jquery-1.11.1.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.backstretch.min.js"></script>
+    <script src="assets/js/retina-1.1.0.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+
+    <!--[if lt IE 10]>
             <script src="assets/js/placeholder.js"></script>
         <![endif]-->
 
-    </body>
+</body>
+
 </html>
--- Employee/CustomerSignUp.php
+++ Employee/CustomerSignUp.php
@@ -8,19 +8,18 @@ session_start();
 ?>
 <?php
 #customer sign sign up data save
-if($_POST)
-{
+if ($_POST) {
 
-$fullname = $_POST["firstname"];
-$email = $_POST["email"];
-$password = $_POST["password"];
+  $fullname = $_POST["firstname"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
 
 
-$res = $pdo->exec("INSERT INTO customer SET fullname='".$fullname."',`email`='".$email."',`password`='".$password."'");
-$cid = $pdo->lastInsertId();
-if($res){
+  $res = $pdo->exec("INSERT INTO customer SET fullname='" . $fullname . "',`email`='" . $email . "',`password`='" . $password . "'");
+  $cid = $pdo->lastInsertId();
+  if ($res) {
 
-echo "<div class='alert alert-success alert-dismissable'>
+    echo "<div class='alert alert-success alert-dismissable'>
 <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>	
 
 Customer Added Successfully!
@@ -28,72 +27,84 @@ Customer Added Successfully!
 </div>
 <meta http-equiv='refresh' content='2; url=CustomerLogin.php?id=$cid' /> 
 ";
-
-
+  }
 }
-
-} 
 ?>
 
 <!DOCTYPE html>
 <html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/customerSignUp.css">
+</head>
 <style>
-input {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+  input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 0px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
 
 
-input[type=submit] {
-  width: 100%;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
+  input[type=submit] {
+    width: 100%;
+    background-color: #0ba360;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
 
-input[type=submit]:hover {
-  background-color: #45a049;
-}
+  input[type=submit]:hover {
+    background-color: #087c49;
+  }
 
-div {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-  width:800px;
-  
-}
+  .signup-model {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+    width: 800px;
 
+  }
 </style>
+
 <body>
+  <div class="background">
+    <div class="container">
+      <div class="form-container">
+        <h1 class="form-title">Customer Sign Up</h1>
+        <div class='signup-model'>
+          <div>
+            <form action="CustomerSignUp.php" method="post">
+              <div class="form-group">
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name..">
+              </div>
+              <div class="form-group">
+                <label for="lname">Email</label>
+                <input type="email" id="email" name="email" placeholder="Your email..">
+              </div>
+              <div class="form-group">
+                <label for="lname">Password</label>
+                <input type="password" id="password" name="password" placeholder="Your Password..">
+              </div>
+              <input type="submit" value="Submit">
+            </form>
+          </div>
+        </div>
+      </div>
 
-<h3>Using CSS to style an HTML Form</h3>
-<div class='signup-model'>
-<div>
-  <form action="CustomerSignUp.php" method="post">
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    </div>
+  </div>
 
-    <label for="lname">Email</label>
-    <input type="email" id="email" name="email" placeholder="Your email..">
-
-    <label for="lname">Password</label>
-    <input type="password" id="password" name="password" placeholder="Your Password..">
-
-    <input type="submit" value="Submit">
-  </form>
-</div>
-</div>
 
 </body>
+
 </html>
-
-
